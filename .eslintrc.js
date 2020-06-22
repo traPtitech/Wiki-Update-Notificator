4038f460ab0a98ba4e6af2fe162f9957771da9de
sapphi-red <green@sapphi.red>
2020-06-22 19:12:07 +0900

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-empty': ['error', { allowEmptyCatch: true }],
+    eqeqeq: 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none'
+        },
+        singleline: {
+          delimiter: 'semi'
+        }
+      }
+    ],
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-extra-semi': 'off'
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+function getProp(key: string) {
+  return PropertiesService.getScriptProperties().getProperty(key)
+}
+function setProp(key: string, val: string) {
+  return PropertiesService.getScriptProperties().setProperty(key, val)
+}
+
+const LAST_UPDATED_PROP_KEY = 'lastUpdated'
+const WEBHOOK_ID = '4495f771-0ad9-4f94-bab2-0f483991bb43'
+const WEBHOOK_SECRET = getProp('WEBHOOK_SECRET')
+const ACCESS_TOKEN = getProp('URL_ENCODED_WIKI_API_TOKEN')
+
+function sendMessage(message: string) {
+  const signature = Utilities.computeHmacSignature(
+    Utilities.MacAlgorithm.HMAC_SHA_1,
+    message,
+    WEBHOOK_SECRET,
+    Utilities.Charset.UTF_8
+  )
+  const sign = signature.reduce((str, ch) => {
+    const chr = (ch < 0 ? ch + 256 : ch).toString(16)
+    return str + (chr.length === 1 ? '0' : '') + chr
+  }, '')
+
+  UrlFetchApp.fetch(`https://q.trap.jp/api/v3/webhooks/${WEBHOOK_ID}`, {
+    method: 'post',
+    contentType: 'text/plain; charset=utf-8',
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+      'X-TRAQ-Signature': sign
+    },
+    payload: message
+  })
+}
+
+function wikiUpdateNotificator() {
+  const URL = `https://wiki.trap.jp/_api/pages.list?access_token=${ACCESS_TOKEN}&path=%2F`
+  const data = UrlFetchApp.fetch(URL)
+  const jsonData = JSON.parse(data.getContentText())
+  const articles = jsonData.pages
+
+  const v = getProp(LAST_UPDATED_PROP_KEY)
+  const lastUpdated = new Date(v)
+
+  articles
+    .filter(article => {
+      const updated = new Date(article.updatedAt)
+      return lastUpdated < updated
+    })
+    .forEach(article => {
+      const text =
+        `${article.revision.author.username}が[${article.path}](https://wiki.trap.jp${article.path})を更新しました\n` +
+        `\n\n` +
+        `> ${article.revision.body.split('\r\n').slice(0, 4).join('\n> ')}`
+      sendMessage(text)
+    })
+
+  setProp(LAST_UPDATED_PROP_KEY, articles[0].updatedAt)
+}
