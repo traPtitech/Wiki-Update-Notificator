df08730345114176ab7e115c294dc191b8fb9fdf
sapphi-red <green@sapphi.red>
2023-07-20 16:56:03 +0900

    update typescript-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
     'plugin:prettier/recommended'
   ],
   rules: {
